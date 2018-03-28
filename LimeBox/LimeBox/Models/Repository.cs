8e195917be8cb57d70d2ecb53ab1a62e021f3488
Marcus D <Marcus@academy.se>
2018-03-28 10:32:26 +0200

    Added Generateboxes

--- LimeBox/LimeBox/Models/Repository.cs
+++ LimeBox/LimeBox/Models/Repository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LimeBox.Models.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,86 @@ namespace LimeBox.Models
 {
     public class Repository
     {
+        private readonly LimeContext context;
+
+        public Repository(LimeContext context)
+        {
+            this.context = context;
+        }
+
+        public void GenerateBoxes(string nameOfBox, int value)
+        {
+
+            //av 100 boxar så är:
+            //standard 80st
+            //premium 15st
+            //Lyx 5st
+            int[] random5 = GenerateRandomNumbers(5);
+            int[] random15 = GenerateRandomNumbers(15, random5);
+            for (int i = 1; i <= 100; i++)
+            {
+                int valueNumber = 1;
+                if (numberIsInArray(random5, i))
+                    valueNumber = 3;
+                else if (numberIsInArray(random15, i))
+
+                context.Add(new Boxes
+                {
+                    BoxId = i,
+                    BoxType = nameOfBox,
+                    BoxValue = valueNumber
+                });
+            }
+        }
+
+        private int[] GenerateRandomNumbers(int amount)
+        {
+            Random random = new Random();
+            int[] numbers = new int[amount];
+
+            for (int i = 0; i < amount; i++)
+            {
+                int randomNumber;
+                do
+                {
+                    randomNumber = random.Next(1, 100 + 1);
+
+                } while (numberIsInArray(numbers, randomNumber));
+
+                numbers[i] = randomNumber;
+            }
+
+            return numbers;
+        }
+
+        private int[] GenerateRandomNumbers(int amount, int[] array)
+        {
+            Random random = new Random();
+            int[] numbers = new int[amount];
+
+            for (int i = 0; i < amount; i++)
+            {
+                int randomNumber;
+                do
+                {
+                    randomNumber = random.Next(1, 100 + 1);
+
+                } while (numberIsInArray(numbers, randomNumber) || numberIsInArray(array, randomNumber));
+
+                numbers[i] = randomNumber;
+            }
+
+            return numbers;
+        }
+
+        private bool numberIsInArray(int[] numbers, int number)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == number)
+                    return true;
+            }
+            return false;
+        }
     }
 }
