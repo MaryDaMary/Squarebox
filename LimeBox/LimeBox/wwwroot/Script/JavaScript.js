38b59d289d308f6e07699d918a23fa490094e467
Marcus D <Marcus@academy.se>
2018-04-03 15:38:48 +0200

    Added removebutton in checkout, and font-family

--- LimeBox/LimeBox/wwwroot/Script/JavaScript.js
+++ LimeBox/LimeBox/wwwroot/Script/JavaScript.js
@@ -1,31 +1,41 @@
-﻿
+﻿//$("data-simplebar2").click(function (event) {
+//    var target = $(event.target);
+//    target.fadeTo("slow", 0.5);
+//});
 
-function RemoveFromCart(id) {
-    alert("Test")
-    $.ajax({
-        url: '/Cart/RemoveFromCart',
-        data: { "id": id },
-        success: function (msg) {
-            alert("Success!")
-            //$("#divResult").html("success");
-        },
-        error: function (e) {
-            alert("Something went wrong!")
-            //$("#divResult").html("Something Wrong.");
-        }
-    });
-};
+
+//$("ShowBox").click(() => {
+//    alert("test");
+//    $(this).fadeTo("slow", 0.33)
+//    $("#ShowBox").fadeTo("slow", 0.5);
+//})
 
 function AddToCart(id) {
     $.ajax({
         url: '/Cart/AddToCart',
         data: { "id": id },
         success: function (msg) {
-            alert("Success!")
+            alert("Success");
             //$("#divResult").html("success");
         },
         error: function (e) {
             console.log(e);
+            alert("Something went wrong!")
+            //$("#divResult").html("Something Wrong.");
+        }
+    });
+};
+
+function RemoveFromCart(id) {
+    $.ajax({
+        url: '/Cart/RemoveFromCart',
+        data: { "id": id },
+        success: function (msg) {
+            alert("Success!")
+            location.reload();
+            //$("#divResult").html("success");
+        },
+        error: function (e) {
             alert("Something went wrong!")
             //$("#divResult").html("Something Wrong.");
         }
